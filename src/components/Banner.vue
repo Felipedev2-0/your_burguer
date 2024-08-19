a96f8886abe1f5dd58e3c98d369989dd153d51a3
Felipe Barros <filbarros@hotmail.com>
2024-08-19 18:19:44 -0300

    Burger Page - VUE.JS

---
+++ src/components/Banner.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="main-banner">
+  <h1>Make Your Burguer</h1>
+</div>
+</template>
+
+<script>
+export default {
+    name:"Banner"
+}
+</script>
+
+<style scoped>
+
+#main-banner{
+    background-image: url(../../public/img/burger.jpg);
+    background-position: 0 -250px;
+    background-size: cover;
+    width: 100%;
+    height: 500px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+}
+
+#main-banner h1{
+  color: #Fff;
+  text-align: center;
+  font-size: 50px;
+  background-color: #222;
+  padding: 20px 40px;
+
+}
+
+</style>
